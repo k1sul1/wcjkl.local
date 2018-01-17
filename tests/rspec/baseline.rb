caf39edf7cc10a16898d035636cbcc47f194bfab
Christian Nikkanen <christian.nikkanen@vincit.com>
2018-01-17 13:36:03 +0200

    Get rid of test

--- tests/rspec/baseline.rb
+++ tests/rspec/baseline.rb
@@ -14,54 +14,6 @@ describe "WordPress: #{WP.siteurl} - ", :type => :request, :js => true do
 
   subject { page }
 
-  describe "frontpage" do
-
-    before do
-      visit WP.siteurl('/')
-    end
-
-    ##
-    # Take a few screenshots of the frontpage for basic external visual regression tests
-    #
-    # These screenshots are used to determine whether the front page has
-    # undergone significant unwanted changes after updates.
-    ##
-    after :all do
-      shots = 0
-      3.times do
-        visit WP.siteurl('/')
-        # All screenshots are accessible from the public url
-        # https://{siteurl}/.seravo/screenshots/frontpage-{0..2}.png
-        save_screenshot "frontpage-#{shots}.png"
-        shots += 1
-      end
-    end
-
-    # 200 Means OK
-    # 503 Means Service Unavailable - For example under construction
-    it "Healthy status code 200, 503" do
-      expect(page).to have_status_of [200,503]
-    end
-
-    it "Page includes stylesheets" do
-      expect(page).to have_css
-    end
-
-    # Example: Check that page has javascript
-    #it "Page includes javascript" do
-    #  expect(page).to have_js
-    #end
-
-    # Example: of link clicking and following to next page
-    #it "After user clicks archive link, User should see archives" do
-    #  click_link('lokakuu 2015')
-    #  expect(page).to have_content 'Kuukausi'
-    #end
-
-    ### Add more customised business critical frontend tests here #####
-
-  end
-
   describe "admin-panel" do
 
     before do
