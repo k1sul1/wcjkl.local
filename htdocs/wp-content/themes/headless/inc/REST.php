f854b9634a1093c1ecaaa66786facd8ef83aacfb
Christian Nikkanen <christian.nikkanen@vincit.com>
2018-01-30 14:23:51 +0200

    Allow CORS in dev too

--- htdocs/wp-content/themes/headless/inc/REST.php
+++ htdocs/wp-content/themes/headless/inc/REST.php
@@ -6,7 +6,12 @@ add_action("rest_api_init", function () {
 
   // Add a Custom filter.
   add_filter('rest_pre_serve_request', function ($value) {
-    header('Access-Control-Allow-Origin: https://pwademo.kisu.li');
+    if (\Vincit\is_prod()) {
+      header('Access-Control-Allow-Origin: https://pwademo.kisu.li');
+    } else {
+      header('Access-Control-Allow-Origin: *');
+    }
+
     header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
     header('Access-Control-Allow-Credentials: true');
 
